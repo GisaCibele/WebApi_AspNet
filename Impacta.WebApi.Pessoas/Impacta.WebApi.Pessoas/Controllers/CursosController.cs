11f7e3706c1cf82b44c5b1329e0ebebf45ec7952
Gisele Cavalcante <gisa.cibele@hotmail.com>
2019-08-17 15:18:45 -0300

    Projeto

---
+++ Impacta.WebApi.Pessoas/Impacta.WebApi.Pessoas/Controllers/CursosController.cs
@@ -0,0 +1,75 @@
+﻿using Impacta.WebApi.Pessoas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Impacta.WebApi.Pessoas.Controllers
+{
+    public class CursosController : ApiController
+    {
+		static List<Curso> ListaCursos = new List<Curso>();
+
+		public List<Curso> GetCursos()
+		{
+			return ListaCursos;
+		}
+
+		public Curso GetCurso(int Id)
+		{
+			var consultaCurso = from c in ListaCursos
+								where c.Id.Equals(Id)
+								select c;
+
+			var res = ListaCursos.Where(x => x.Id.Equals(Id)).FirstOrDefault();
+
+			if (consultaCurso.Count() <= 0)
+			{
+				return null;
+			}
+			else
+			{
+				return consultaCurso.First();
+				}
+			}
+
+		public void PostCurso(Curso curso)
+		{
+			if (curso != null)
+			{
+				ListaCursos.Add(curso);
+			}
+		}
+
+		public void PutCurso(int id, Curso curso)
+		{
+			if (curso != null && id > 0)
+			{
+				var result = ListaCursos.Where(x => x.Id.Equals(id)).FirstOrDefault();
+
+
+				result.Nome = curso.Nome;
+				result.CargaHoraria = curso.CargaHoraria;
+
+				int posicao = ListaCursos.IndexOf(result);
+
+				ListaCursos.RemoveAt(posicao);
+				ListaCursos.Insert(posicao, curso);
+			}
+		}
+
+		public List<Curso> DeleteCurso(int id)
+		{
+			if (id > 0)
+			{
+				ListaCursos.RemoveAt(
+					ListaCursos.IndexOf(
+						ListaCursos.Where(x => x.Id.Equals(id)).FirstOrDefault()
+					));
+			}
+			return ListaCursos;
+		} 
+	}
+ }
---
+++ Impacta.WebApi.Pessoas/Impacta.WebApi.Pessoas/Controllers/PessoasController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Impacta.WebApi.Pessoas.Models;
+
+namespace Impacta.WebApi.Pessoas.Controllers
+{
+	public class PessoasController : ApiController
+	{
+		static List<Pessoa> pessoas = new List<Pessoa>();
+		private string nomeDaPessoa;
+
+		public PessoasController()
+		{
+
+		}
+
+		public List<Pessoa> Get()
+		{
+			return pessoas;
+		}
+
+		public void Post(string nomeDaPassoa)
+		{
+			if (!string.IsNullOrEmpty(nomeDaPessoa))
+			{
+				pessoas.Add(new Pessoa { Nome = nomeDaPassoa });
+			}
+		}
+
+		public void post(Pessoa pessoa)
+		{
+			if (pessoa != null)
+			{
+				pessoas.Add(pessoa);
+			}
+		}
+	}
+}
